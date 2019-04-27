afcd2488d2fedde56b345a54b26d6fe0f7086a41
wl <949758173@qq.com>
2019-04-27 21:16:04 +0800

    提交模板方法模式代码,提交Netty编解码器学习笔记

---
+++ design-pattern/src/main/java/test/pattern/templatemethod/ConcreteClass.java
@@ -0,0 +1,25 @@
+/**
+ * FileName: test.pattern.templatemethod
+ * Author    王磊
+ * Date      2019/4/27 21:14
+ * Description: 具体实现类，实现原语操作
+ */
+package test.pattern.templatemethod;
+
+/**
+ * 具体实现类，实现原语操作
+ *
+ * @author Administrator
+ * @since 1.0.0
+ */
+public class ConcreteClass extends AbstractClass {
+    @Override
+    public void doPrimitiveOperation1() {
+
+    }
+
+    @Override
+    public void doPrimitiveOperation2() {
+
+    }
+}

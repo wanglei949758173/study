9941e853e6e378614679e11dbfec244ff0802b3f
wl <949758173@qq.com>
2019-04-29 22:20:45 +0800

    提交命令模式代码

---
+++ design-pattern/src/main/java/test/pattern/command/Receiver.java
@@ -0,0 +1,22 @@
+/**
+ * FileName: test.pattern.command
+ * Author    王磊
+ * Date      2019/4/29 22:18
+ * Description: 接收者对象
+ */
+package test.pattern.command;
+
+/**
+ * 接收者对象
+ *
+ * @author Administrator
+ * @since 1.0.0
+ */
+public class Receiver {
+    /**
+     * 示意方法，真正执行命令相应的操作
+     */
+    public void action() {
+        //真正执行命令操作的功能代码
+    }
+}

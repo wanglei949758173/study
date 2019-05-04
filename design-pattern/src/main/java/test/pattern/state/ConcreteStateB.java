54e1e59bbb4f82174617bf2af2b56fe4afafd500
wl <949758173@qq.com>
2019-05-04 20:24:11 +0800

    提交状态模式代码

---
+++ design-pattern/src/main/java/test/pattern/state/ConcreteStateB.java
@@ -0,0 +1,20 @@
+/**
+ * FileName: test.pattern.state
+ * Author    王磊
+ * Date      2019/5/4 20:12
+ * Description: 实现一个与Context的一个特定状态相关的行为
+ */
+package test.pattern.state;
+
+/**
+ * 实现一个与Context的一个特定状态相关的行为
+ *
+ * @author Administrator
+ * @since 1.0.0
+ */
+public class ConcreteStateB implements State {
+    @Override
+    public void handle(String sampleParameter) {
+        //实现具体的处理
+    }
+}

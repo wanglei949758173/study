4f4a696fab170916361906c080ab0d188e6211e1
wl <949758173@qq.com>
2019-05-06 21:19:25 +0800

    提交策略模式代码

---
+++ design-pattern/src/main/java/test/pattern/strategy/ConcreteStrategyB.java
@@ -0,0 +1,20 @@
+/**
+ * FileName: test.pattern.strategy
+ * Author    王磊
+ * Date      2019/5/6 21:17
+ * Description: 实现具体的算法
+ */
+package test.pattern.strategy;
+
+/**
+ * 实现具体的算法
+ *
+ * @author Administrator
+ * @since 1.0.0
+ */
+public class ConcreteStrategyB implements Strategy {
+    @Override
+    public void algorithmInterface() {
+
+    }
+}

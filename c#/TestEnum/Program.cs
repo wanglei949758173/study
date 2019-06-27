67749b629b97e6a17609449fa5d2b41a34b0fcec
wl <949758173@qq.com>
2019-06-27 21:31:11 +0800

    wpf记事本和c#基础

---
+++ c#/TestEnum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEnum
+{
+    enum Gender
+    {
+        Male, Female
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gender zhangsan = Gender.Male;
+            Gender xiaohong = Gender.Female;
+
+            String[] names = {"zhangsan", "lisi", "wangwu"};
+            foreach(String name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c#/TestParams/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestParams
+{
+    /// <summary>
+    /// 测试可变参数
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printNickNames("zhangsan", "狗蛋", "老张", "小三");
+            Console.ReadKey();
+        }
+
+        static void printNickNames(String realName, params String[] nickNames)
+        {
+            Console.WriteLine("{0}的昵称有:", realName);
+            foreach (String nickName in nickNames)
+            {
+                Console.WriteLine("     {0}", nickName);
+            }
+        }
+    }
+}

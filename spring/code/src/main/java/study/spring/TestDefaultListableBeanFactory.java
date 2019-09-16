6a04bcecdd0b89251be08380510efdd518135058
wl <949758173@qq.com>
2019-09-16 22:01:38 +0800

    bean definition & xmlBeanDefinitionReader

--- spring/code/src/main/java/study/spring/TestDefaultListableBeanFactory.java
+++ spring/code/src/main/java/study/spring/TestDefaultListableBeanFactory.java
@@ -4,34 +4,41 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 
+import java.util.Map;
+
 /**
  * 测试DefaultListableBeanFactory
- * 
- * @author Administrator
  *
+ * @author Administrator
  */
 public class TestDefaultListableBeanFactory {
-	public static void main(String[] args) {
-		// 定义spring配置文件
-		// <bean name="person" class="study.spring.Person"></bean>
+    public static void main(String[] args) {
+        // 定义spring配置文件
+        // <bean name="person" class="study.spring.Person"></bean>
 
-		// 创建工厂
-		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        // 创建工厂
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
-		// 创建resource
-		ClassPathResource resource = new ClassPathResource("testDefaultListableBeanFactory.xml");
+        // 创建resource
+        ClassPathResource resource = new ClassPathResource("testDefaultListableBeanFactory.xml");
 
-		// 创建Reader
-		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        // 创建Reader
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
 
-		// 从resource加载BeanDefinition
-		reader.loadBeanDefinitions(resource);
+        // 从resource加载BeanDefinition
+        reader.loadBeanDefinitions(resource);
 
-		// 获取Bean
-		Person person = beanFactory.getBean(Person.class);
-		System.out.println(person);
-	}
+        // 打印调用堆栈
+        Map<Thread, StackTraceElement[]> allStackTraces = Thread.getAllStackTraces();
+        StackTraceElement[] stackTraceElements = allStackTraces.get(Thread.currentThread());
+        for (StackTraceElement s : stackTraceElements) {
+            System.out.println("------------------------" + s.getClassName() + "$" + s.getMethodName());
+        }
 
-	
+        // 获取Bean
+        Person person = beanFactory.getBean(Person.class);
+        System.out.println(person);
+    }
+
 
 }

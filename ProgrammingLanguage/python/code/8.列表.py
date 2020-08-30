067b84411b993a240d0210bb506a494592b26614
wanglei <949758173@qq.com>
2020-08-30 22:33:21 +0800

    python列表

--- ProgrammingLanguage/python/code/8.列表.py
+++ ProgrammingLanguage/python/code/8.列表.py
@@ -26,3 +26,33 @@ print(myList[0:5:2]); # 10, Hello, False
 
 # 当步长为负数时,则从后往前取
 print(myList[::-2]); # False, 'Hello', 10
+
+# 列表合并
+myList = [1,2,3] + [4,5,6];
+print(myList);# 1,2,3,4,5,6
+
+# * 可以将列表重复指定的次数
+myList = [1,2,3] * 6;
+print(myList); # 1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3
+
+# in & not in
+myList = ['张三', '李四', '王五', '赵六'];
+zhangsanIsIn = '张三' in myList;
+print(zhangsanIsIn);# True
+print('张三' not in myList);# False
+
+# 求列表长度
+print(len(myList)); # 4
+
+# 求最小值最大值
+myList = [2,1,3,4,5]
+print(min(myList));# 1
+print(max(myList));# 5
+
+# 获取元素索引
+print(myList.index(3));# 2
+
+# 求元素出现的个数
+myList = ['张三', '李四', '张三', '王五'];
+print(myList.count('张三'));# 2
+
